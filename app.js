aa2f1d1f8d99668345a7031d92b05b587976b5ed
anuragjos <88484101+anuragjos@users.noreply.github.com>
2023-04-06 15:58:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
   // Sending This is the home page! in the page
   res
     .status(200)
-    .send(`This is My Sample Node.js CICD Pipeline Setup `);
+    .send(`This is My Node.js Web Application CICD Pipeline Setup `);
 });
 
 // Listening to the port 
